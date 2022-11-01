dd347f31693c4208acb2ef22d3104caee6d2c388
Aleksandr Nikolaev <aleksandr.nikolaev@ihsmarkit.com>
2022-11-01 18:33:48 +0600

    BinarySearch attempt 2

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -1,34 +1,22 @@
 using System;
-using System.Linq;
 public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        var currentArray = input;
-        var delta = 0;
-        while (currentArray.Length > 1)
+        var min = 0;
+        var max = input.Length - 1;
+        while (min < max)
         {
-            var count = currentArray.Length;
-            var middleIndex = count / 2;
-
-            var comparisonValue = currentArray[middleIndex];
-
-            if (comparisonValue == value)
-                return middleIndex + delta;
-
-            var leftPart = comparisonValue > value;
-            
-            var newArray  = currentArray.Select((Value, Index) => new {Value, Index})
-                .Where((arg => leftPart ? (arg.Index < middleIndex) : arg.Index > middleIndex)).Select(arg => arg.Value).ToArray();
-
-            if (!leftPart)
-                delta += currentArray.Length - newArray.Length;
-            
-            currentArray = newArray;
-
+            var currentIndex = (max + min) / 2;
+            var currentValue = input[currentIndex];
+            if (currentValue > value)
+                max = currentIndex - 1;
+            else if (currentValue < value)
+                min = currentIndex + 1;
+            else
+                return currentIndex;
         }
-
-        return (currentArray.Length != 0 && currentArray[0] == value) ? delta : -1;
-
+        
+        return input.Length > 0 && min == max && input[min] == value ? min : -1;
     }
 }
