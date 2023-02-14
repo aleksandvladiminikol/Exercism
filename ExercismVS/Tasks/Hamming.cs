1656287d75fc5f39dc8be95c5478021eb6364308
Aleksandr Nikolaev <aleksandr.nikolaev@ihsmarkit.com>
2023-02-14 19:02:35 +0600

    Hamming

---
+++ ExercismVS/Tasks/Hamming.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length != secondStrand.Length)
+            throw new ArgumentException("Strands lentgh should be equal!");
+        return Enumerable.Zip(firstStrand, secondStrand).Where(_ => _.First != _.Second).Count();
+    }
+}
+
